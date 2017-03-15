d07c2436559f173860e98074a0b11e565eb9a1fc
David Cramer <david@digilab.co.za>
2017-03-15 07:19:51 +0200

    init docs

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","TourPlan_API"],["c","TourPlan_Dashboard"],["f","tourplan_dashboard()"],["f","tourplan_dashboard_autoload_class()"],["f","tourplan_dashboard_php_ver()"]];
